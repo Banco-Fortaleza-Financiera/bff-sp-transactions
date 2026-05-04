1bee635a8095783fa108bcde9722fd775e659b37
MS-Marcks <gersonramos46@gmail.com>
2026-05-04 08:26:22 -0600

    feat: enable caching in TransactionsApplication

--- src/main/java/com/bancofortaleza/transactions/TransactionsApplication.java
+++ src/main/java/com/bancofortaleza/transactions/TransactionsApplication.java
@@ -2,7 +2,9 @@ package com.bancofortaleza.transactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class TransactionsApplication {
 
